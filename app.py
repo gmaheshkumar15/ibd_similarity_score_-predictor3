f192868beaa67a44dcc12c9895cf74c6277fd995
gmaheshkumar15 <gmaheshk85@gmail.com>
2025-11-22 15:50:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,8 +75,9 @@ with col_title:
         unsafe_allow_html=True
     )
 
+
 with col_logo_right:
-    st.markdown('<img src="https://tse2.mm.bing.net/th/id/OIP.fNb1hJAUj-8vwANfP3SDJgAAAA?pid=Api&P=0&h=180" class="logo-right">', unsafe_allow_html=True)
+    st.markdown('<img src="https://raw.githubusercontent.com/gmaheshkumar15/ibd_similarity_score_-predictor3/main/dmch.jpeg" class="logo-right">', unsafe_allow_html=True)
     st.markdown('<div class="institute-name">Dayanand Medical College and Hospital Ludhiana</div>', unsafe_allow_html=True)
 
 # -----------------------------
